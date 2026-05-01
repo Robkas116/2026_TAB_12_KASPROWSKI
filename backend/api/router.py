179c22147f898e003d649c77cf047e19fb7bdc9f
l2skowron <146718756+l2skowron@users.noreply.github.com>
2026-05-01 11:07:31 +0200

    Feature/dev based reservation (#6)
    * Bringing changes from feature/reservation branch
    
    * Reservation update
    
    Finalizing reservation.
    
    * Update reservation //NOTE without vehicle
    
    * Ruff checks
    
    ---------
    
    Co-authored-by: Robkas116 <rkasprowski@onet.pl>

--- backend/api/router.py
+++ backend/api/router.py
@@ -8,6 +8,7 @@ from api.routes import (
     version_endpoints,
     action_endpoints,
     is_performed_endpoints,
+    reservation_endpoints,
 )
 
 api_router = APIRouter()
@@ -18,3 +19,4 @@ api_router.include_router(set_of_equipment_endpoints.router)
 api_router.include_router(version_endpoints.router)
 api_router.include_router(action_endpoints.router)
 api_router.include_router(is_performed_endpoints.router)
+api_router.include_router(reservation_endpoints.router)
